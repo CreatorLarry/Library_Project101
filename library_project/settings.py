8531dc4e980e2ea7c79ad58fa5108ff76a506975
unknown <CreatorLarry>
2025-01-14 22:43:44 +0300

    pushing for real

--- library_project/settings.py
+++ library_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ddjp#(!7^q&(o_(+ikkf66rcwq%-(o)yck$%$qt0(uud5ix1s+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
